e2a052164c8f5d1e2726fe9b4617c8044af67b4a
work <nizamovaarofat@gmail.com>
2025-01-16 13:39:53 +0500

    added page silk-road

--- main.js
+++ main.js
@@ -172,6 +172,14 @@
 		  }
 		]
 	  });
+	  $('.slider-countries').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: false,
+	  });
 	  function truncateText(selector, maxLength) {
 		const elements = document.querySelectorAll(selector);
 	
