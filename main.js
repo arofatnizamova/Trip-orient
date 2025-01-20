6cd744c003b0e6d1cbade34f410fc492c1973880
work <nizamovaarofat@gmail.com>
2025-01-20 17:08:19 +0500

    validated pages

--- main.js
+++ main.js
@@ -1,200 +1,171 @@
 
-  $(document).ready(function() {
-	$('.slider').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: false,
-		arrows:true,
-		prevArrow: $('.prev'),
-		nextArrow: $('.next'),
-		responsive: [{
-			breakpoint: 992,
-				settings: {
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-				  arrows: false,
-				}
-		}
-			
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	  });
-	  $('.slider-service').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: false,
-		arrows:true,
-		prevArrow: $('.prev-service'),
-		nextArrow: $('.next-service'),
-		responsive: [{
-			breakpoint: 992,
-				settings: {
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-				  arrows: false,
-				}
-		}
-			
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	  });
-	$('.slider-items').slick({
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 4,
-	  slidesToScroll: 1,
-	  dots: false,
-	  arrows: false,
-	  responsive: [
-		{
-		  breakpoint: 990,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: true,
-			prevArrow: $('.prev-item'),
-			nextArrow: $('.next-item'),
-		  }
-		}
-	  ]
-	});
-	$('.slider-tours').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1.8,
-		slidesToScroll: 1,
-		centerMode: false,
-		dots: true,
-		arrows: false,
-		responsive: [
-			{
-				breakpoint: 1440,
-				settings: {
-				  slidesToShow: 1.9,
-				  slidesToScroll: 1,
-				}
-			  },
-		  {
-			breakpoint: 990,
-			settings: {
-			  slidesToShow: 1,
-			  centerMode: true,
-			  slidesToScroll: 1,
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  centerMode: false,
-			  slidesToScroll: 1,
-			}
-		  }
-		]
-	  });
-	  $('.slider-cards').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 3.9,
-		slidesToScroll: 1,
-		centerMode: false,
-		dots: false,
-		arrows: true,
-		prevArrow: $('.prev-card'),
-		nextArrow: $('.next-card'),
-		responsive: [
-			{
-				breakpoint: 1440,
-				settings: {
-				  slidesToShow: 2.9,
-				  slidesToScroll: 1,
-				}
-			  },
-		  {
-			breakpoint: 990,
-			settings: {
-			  slidesToShow: 1,
-			  centerMode: true,
-			  slidesToScroll: 1,
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  centerMode: false,
-			  arrows: false,
-			  slidesToScroll: 1,
-			}
-		  }
-		]
-	  });
-	  
-	  $('.slider-testimonials').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1.9,
-		slidesToScroll: 1,
-		centerMode: true,
-		dots: true,
-		arrows: false,
-		responsive: [
-			{
-				breakpoint: 1440,
-				settings: {
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-				}
-			  },
-		  {
-			breakpoint: 990,
-			settings: {
-			  slidesToShow: 1,
-			  centerMode: true,
-			  slidesToScroll: 1,
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  centerMode: false,
-			  slidesToScroll: 1,
-			}
-		  }
-		]
-	  });
-	  $('.slider-countries').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		arrows: false,
-	  });
-	  function truncateText(selector, maxLength) {
-		const elements = document.querySelectorAll(selector);
-	
-		elements.forEach(el => {
-			if (el.innerText.length > maxLength) {
-				el.innerText = el.innerText.slice(0, maxLength) + '...';
-			}
-		});
+$(document).ready(function () {
+
+	$('.slick-slider').each(function () {
+    let slider = $(this); 
+    let options = {
+        infinite: true,
+        prevArrow: slider.parent().find('.slider-prev'),
+        nextArrow: slider.parent().find('.slider-next'),
+    };
+    let extra = {};
+    if (slider.hasClass('slider')) {
+        extra = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            responsive: [{
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                },
+            }]
+        };
+    } else if (slider.hasClass('slider-items')) {
+        extra = {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            responsive: [{
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                },
+            }],
+        };
+    }else if (slider.hasClass('slider-tours')) {
+        extra = {
+            slidesToShow: 1.8,
+            slidesToScroll: 1,
+            centerMode: false,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 1.9,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 990,
+                    settings: {
+                        slidesToShow: 1.2,
+                        centerMode: false,
+                        slidesToScroll: 1,
+                    },
+				},
+				{
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        centerMode: false,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        };
+    }else if (slider.hasClass('slider-service')) {
+        extra = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            responsive: [{
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                },
+            }],
+        };
+    }else if (slider.hasClass('slider-cards')) {
+        extra = {
+           slidesToShow: 3.9,
+            slidesToScroll: 1,
+            centerMode: false,
+            dots: false,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 2.9,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 990,
+                    settings: {
+                        slidesToShow: 1,
+                        centerMode: true,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        centerMode: false,
+                        arrows: false,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        };
+    }else if (slider.hasClass('slider-testimonials')) {
+        extra = {
+           slidesToShow: 1.9,
+            slidesToScroll: 1,
+            centerMode: true,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 990,
+                    settings: {
+                        slidesToShow: 1,
+                        centerMode: true,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+                        centerMode: false,
+                    },
+                },
+            ],
+        };
+	} else if (slider.hasClass('slider-countries')) {
+		extra = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+        };
 	}
+    slider.slick($.extend({}, options, extra));
+});
+
+   
+
+
 	
-	// Обрезка текста на мобильных устройствах
-	if (window.innerWidth <= 768) {
-		truncateText('.truncated-text', 150); // Например, обрезать до 50 символов
-	}
-	
+
   });
 
   
